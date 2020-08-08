d7fb8a317d31eef77d54fd4c55d73aabf6d6f781
Victor Green <thevictorgreen@gmail.com>
2020-08-08 16:16:18 -0400

    Initial Code Base

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+package io.test;
+import java.util.*;
+//ALGORITHM SPECIFICATION AND IMPLEMENTATION
+public class Test {
+  //BEGIN ALGORITHM SPECIFICATION
+
+  //END ALGORITHM SPECIFICATION
+  public Test() {
+    //ALGORITHM IMPLEMENTATION
+    System.out.println("Executing Algorithm\n");
+  }
+  public static void main(String[] args) {
+    new Test();
+  }
+}
